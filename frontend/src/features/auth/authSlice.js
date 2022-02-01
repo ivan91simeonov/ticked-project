ff818ef64b68f1415393206af448fe11bd79c980
ivan91simeonov <vanches91@abv.bg>
2022-02-01 19:49:44 +0200

    Logout User

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -37,6 +37,12 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
   console.log(user);
 });
 
+// Logout user
+
+export const logout = createAsyncThunk('auth/logout' ,async () => {
+    await authService.logout
+})
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -63,6 +69,9 @@ export const authSlice = createSlice({
         state.isError = true;
         state.message = action.payload;
         state.user = null;
+      })
+      .addCase(logout.fulfilled , (state) => {
+        state.user = null
       })
   },
 });
