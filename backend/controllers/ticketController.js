c5c9e00dbe4d3d7dd96ea3a47f10a113c40c03fc
ivan91simeonov <vanches91@abv.bg>
2022-02-03 20:20:32 +0200

    Update and Delete Ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -1,61 +1,152 @@
 const asyncHandler = require('express-async-handler');
 
-const User = require('../models/userModel')
+const User = require('../models/userModel');
 const Ticket = require('../models/ticketModel');
 const { protect } = require('../middleware/authMiddleware');
 
-
-//@desc Get user tickets 
+//@desc Get user tickets
 // @route Get /api/tickets
 // @access Private
 
 const getTickets = asyncHandler(async (req, res) => {
-    // get user using the Id in the JWT
-    const user = await User.findById(req.user.id)
+  // get user using the Id in the JWT
+  const user = await User.findById(req.user.id);
 
-    if(!user) {
-        res.status(401)
-        throw new Error('User not found')
-    }
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
 
-    const ticket = await Ticket.find({user: req.user.id})
+  const ticket = await Ticket.find({ user: req.user.id });
 
-    res.status(200).json(ticket) 
-  })
+  res.status(200).json(ticket);
+});
 
-  //@desc Create new  ticket
+//@desc Get user ticket
+// @route Get /api/tickets/:id
+// @access Private
+
+const getTicket = asyncHandler(async (req, res) => {
+  // get user using the Id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  res.status(200).json(ticket);
+});
+
+//@desc Delete ticket
+// @route Delete /api/tickets/:id
+// @access Private
+
+const deleteTicket = asyncHandler(async (req, res) => {
+  // get user using the Id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  await ticket.remove();
+
+  res.status(200).json({ success: true });
+});
+
+//@desc Update  ticket
+// @route PUT /api/tickets/:id
+// @access Private
+
+const updateTicket = asyncHandler(async (req, res) => {
+  // get user using the Id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
+//@desc Create new  ticket
 // @route Post /api/ticket
 // @access Private
 
 const createTicket = asyncHandler(async (req, res) => {
-    const {product , description } = req.body
+  const { product, description } = req.body;
 
-    if(!product || !description ) {
-        res.status(400)
-        throw new Error('Please add a product and description')
-    }
-
-    const user = await User.findById(req.user.id)
-
-    if(!user) {
-        res.status(401)
-        throw new Error('User not found')
-    }
-
-    const ticket = await Ticket.create({
-        product,
-        description,
-        user: req.user.id,
-        status: 'new'
-    })
-
-
-
-    res.status(201).json(ticket) 
-  })
-  
-
-  module.exports = {
-    getTickets,
-    createTicket
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('Please add a product and description');
   }
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new',
+  });
+
+  res.status(201).json(ticket);
+});
+
+module.exports = {
+  getTickets,
+  createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket
+};
